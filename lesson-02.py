29f9654d7acd2e2e487315a13a9771ca812ee05e
Perrin Samuels <perrin.samuels@gmail.com>
2019-01-02 10:33:20 -0800

    add lesson 02

--- lesson-02.py
+++ lesson-02.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+# lesson-02.py - show a $10 withdrawal and a $20 deposit
+#
+# Learn:
+#     print empty line
+#     compound assignment operators
+
 balance = 100
 print('Welcome to the bank.')
 print('Your balance is $%d.' % balance)
