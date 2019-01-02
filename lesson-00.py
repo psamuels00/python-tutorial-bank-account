8a23f6acb8fc43cd0d26aa55ba5300f02df6c6c2
Perrin Samuels <perrin.samuels@gmail.com>
2019-01-02 10:23:22 -0800

    add lesson 00

---
+++ lesson-00.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# lesson-00.py - print hello world
+#
+# Learn:
+#     print statement
+#     string literal
+#     shebang line
+#     single-line comments
+
+print('Hello, world.')
